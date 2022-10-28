502de86b95ee9a4f4859aae7e0806590dca3472e
Olayemi <80619568+yemola@users.noreply.github.com>
2022-10-28 21:59:20 +0100

    Update my.js

--- routes/my.js
+++ routes/my.js
@@ -15,14 +15,4 @@ router.get("/listings", auth, (req, res) => {
 
 module.exports = router;
 
-// {
-//   "assetsBaseUrl": "https://donebucket1.s3.eu-central-1.amazonaws.com/",
-//   "aws-bucket-name": "donebucket1",
-//   "aws-bucket-region": "eu-central-1",
-//   "aws-access-key": "AKIATFLS4RXOWQNHX6CD",
-//   "aws-secret-key": "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",
-//   "mongoUrl": "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.yygdw.mongodb.net/DoneWithIt?retryWrites=true&w=majority",
-//   "PASS_SEC": "yem223",
-//   "JWT_SEC": "yem223",
-//   "STRIPE_KEY": "sk_test_51KpDFcHMRCPcYjAQthN2GQRvpgnPmN3DildVoqm3RAR4Px65nB2lfkMOySxoIsrDtf54wdNaqiml68OW2OebCgoK00K95VZRgd"
-// }
+
