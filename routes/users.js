8be7055eac5a13b055e6ed8fe08a6f5c1ed4bcb2
Olayemi <yemolaolu@gmail.com>
2023-01-09 14:48:19 +0100

    mailOptions updated

--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,9 @@ const emailSchema = yup.object().shape({
 
 // Nodemailer Transporter
 let transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
